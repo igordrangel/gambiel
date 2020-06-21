eb207daf295ad5a4dfa2e2cf4a2f1d4d90dd435a
igordrangel <igor.rangel@ciadocredito.net.br>
2020-06-20 21:10:00 -0300

    Iniciando Projeto

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# gambiel
+# GambiEl
+
+## Install
+```
+composer require igordrangel/gambiel
+```
+
+### Usage
+```bash
+$query = ["id" => ""];
+$data = GambiEl::new(
+    GambiEl::add("id","1")
+    GambiEl::add("name","Igor")
+    GambiEl::add("status",true)
+);
+$result = GambiEl::query($data, $query);
+printr($result); // ["id" => "1"]
+```
---
+++ src/GambiEl.php
@@ -0,0 +1,85 @@
+<?php
+	declare(strict_types=1);
+	
+	class GambiEl {
+		/**
+		 * Filter data by query Array
+		 * @param array $data Data creating by new method on GambiEl.new
+		 * @param array|null $query
+		 * @return array
+		 * @link https://github.com/igordrangel/gambiel/blob/master/README.md
+		 */
+		public static function query(array $data, array $query = null): array {
+			if (is_array($query)) {
+				foreach ($data as $indexDado => $dado) {
+					if (empty($query) && is_array($dado)) {
+						if (!empty(current($dado)) && is_array(current($dado))) {
+							foreach ($dado as $item) {
+								if (empty($query)) {
+									$data[$indexDado] = [];
+									if (!empty(array_key_first($item))) {
+										array_push($data[$indexDado], [
+											array_key_first($item) => current($item)
+										]);
+									}
+								} else {
+									$data[$indexDado] = self::query($item, $query[$indexDado] ? $query[$indexDado] : null);
+								}
+							}
+						} else {
+							if (!empty(array_key_first($dado))) {
+								$data[$indexDado] = [
+									array_key_first($dado) => current($dado)
+								];
+							}
+						}
+					} else {
+						if (is_array($dado) && array_key_exists($indexDado, $query)) {
+							if (is_array(current($dado))) {
+								$data[$indexDado] = [];
+								foreach ($dado as $item) {
+									array_push($data[$indexDado], self::query($item, $query[$indexDado] ? $query[$indexDado] : null));
+								}
+							} else {
+								$data[$indexDado] = self::query($dado, $query[$indexDado] ? $query[$indexDado] : null);
+							}
+						} else {
+							if (!empty($query) && !array_key_exists($indexDado, $query)) {
+								unset($data[$indexDado]);
+							}
+						}
+					}
+				}
+			}
+			
+			return $data;
+		}
+		
+		/**
+		 * Create array for quering on GabiEl::query
+		 * @param array $data List items add by GambiEl::add
+		 * @return array
+		 * @link https://github.com/igordrangel/gambiel/blob/master/README.md
+		 */
+		public static function new(array ...$data): array {
+			$dadosParaMerge = [];
+			foreach ($data as $value) {
+				foreach ($value as $index => $item) {
+					$dadosParaMerge[$index] = $item;
+				}
+			}
+			return array_filter(array_merge($dadosParaMerge), function ($var): bool { return $var !== '!!not-allowed!!'; });
+		}
+		
+		/**
+		 * Add item on array
+		 * @param string $name
+		 * @param $value
+		 * @param bool $havePermission Permission to show this value on return
+		 * @return array
+		 * @link https://github.com/igordrangel/gambiel/blob/master/README.md
+		 */
+		public static function add(string $name, $value, bool $havePermission = true): array {
+			return [$name => $havePermission ? $value : '!!not-allowed!!'];
+		}
+	}
